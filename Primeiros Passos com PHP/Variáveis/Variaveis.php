a585ecdae6e32b8b70ff2c13d1c116d1f1c26b72
LuccasArruda <luccasarruda@hotmail.com.br>
2023-04-09 23:20:50 -0300

    Primeiros Passos - Variáveis

---
+++ Primeiros Passos com PHP/Variáveis/Variaveis.php
@@ -0,0 +1,13 @@
+<?php
+
+$LNum1 = 2;
+$LNum2 = 3;
+
+$LSoma = $LNum1 + $LNum2;
+$LSubtracao = $LNum1 - $LNum2;
+$LMultiplicacao = $LNum1 * $LNum2;
+$LDivisao = $LNum2 / $LNum1;
+$LPotencia = $LNum1 ** $LNum2;
+$LRestoDivisao = $LNum1 % $LNum2;
+
+echo $LSoma.' | '.$LSubtracao.' | '.$LMultiplicacao.' | '.$LDivisao.' | '.$LPotencia.' | '.$LRestoDivisao;
